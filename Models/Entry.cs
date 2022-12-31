9b2fd88848825a80ef402f2b0f6bf2dcbfaf8a53
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2022-12-31 17:22:58 +0300

    deine column type for decimal fields

--- Models/Entry.cs
+++ Models/Entry.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace JobRecommendationApp.Models
 {
     public class Entry
     {
         public int Id { get; set; }
+        [Column(TypeName = "decimal(2,1)")]
         public decimal Realistic { get; set; }
+        [Column(TypeName = "decimal(2,1)")]
         public decimal Investigation { get; set; }
+        [Column(TypeName = "decimal(2,1)")]
         public decimal Artistic { get; set; }
+        [Column(TypeName = "decimal(2,1)")]
         public decimal Social { get; set; }
-        public decimal Enterprising  { get; set; }
+        [Column(TypeName = "decimal(2,1)")]
+        public decimal Enterprising { get; set; }
+        [Column(TypeName = "decimal(2,1)")]
         public decimal Conventional { get; set; }
+        [Column(TypeName = "decimal(2,1)")]
         public bool IsSatisfied { get; set; }
-    }    
+    }
 }
