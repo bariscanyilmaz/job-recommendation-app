c137dd1765a34e7fb22de377f7e1dcfb22d491a8
Barış Can Yılmaz <30300440+bariscanyilmaz@users.noreply.github.com>
2023-01-04 10:55:14 +0300

    change decimal column precision

--- Models/Entry.cs
+++ Models/Entry.cs
@@ -5,19 +5,19 @@ namespace JobRecommendationApp.Models
     public class Entry
     {
         public int Id { get; set; }
-        [Column(TypeName = "decimal(2,1)")]
+        [Column(TypeName = "decimal(18,1)")]
         public decimal Realistic { get; set; }
-        [Column(TypeName = "decimal(2,1)")]
+        [Column(TypeName = "decimal(18,1)")]
         public decimal Investigation { get; set; }
-        [Column(TypeName = "decimal(2,1)")]
+        [Column(TypeName = "decimal(18,1)")]
         public decimal Artistic { get; set; }
-        [Column(TypeName = "decimal(2,1)")]
+        [Column(TypeName = "decimal(18,1)")]
         public decimal Social { get; set; }
-        [Column(TypeName = "decimal(2,1)")]
+        [Column(TypeName = "decimal(18,1)")]
         public decimal Enterprising { get; set; }
-        [Column(TypeName = "decimal(2,1)")]
+        [Column(TypeName = "decimal(18,1)")]
         public decimal Conventional { get; set; }
-        [Column(TypeName = "decimal(2,1)")]
+        [Column(TypeName = "decimal(18,1)")]
         public bool IsSatisfied { get; set; }
     }
 }
